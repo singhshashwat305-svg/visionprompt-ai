ef6543d9e93511cf1b659a88cbbf01077b2d4ff8
Shashwat Singh <singhshashwat305@gmail.com>
2026-07-04 23:21:21 +0530

    Clean repository by removing unrelated files

--- addition_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.title('Addition app')
-
-a=st.number_input('Enter the first number')
-b=st.number_input('Enter the second number')
-
-if st.button('Add'):
-    c= a+b
-    st.success(f'the addition of {a} and {b} is: {c}')
